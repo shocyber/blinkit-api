56c7a57386480e7939583bfa7f85e9d8e3e670d4
Mullayam <50077856+Mullayam@users.noreply.github.com>
2023-08-26 21:18:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const routes = require("./routes");
+// const routes = require("./routes");
 app.use(cors({ origin: "*", methods: ["GET", "POST"] }));
 app.use(express.json())
-app.use(routes);
+// app.use(routes);
 
-// app.listen(5000, () => {
-//   console.log("http://localhost:5000");
-// });
+app.listen(5000, () => {
+  console.log("http://localhost:5000");
+});
